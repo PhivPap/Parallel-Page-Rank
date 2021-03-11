c3bc81acf99c1f48e35dea9ecca2b2c3edd0add5
Phivos Papapanagiotakis <csd3823@csd.uoc.gr>
2021-03-11 19:03:56 +0200

    Switched to int64 for the graph indexing. Added functions to print page ranks and implemented a basic form of the single threaded page rank algorithm.

--- ParalPageRank.c
+++ ParalPageRank.c
@@ -4,8 +4,10 @@
 
 #define INIT_GRAPH_NODES 1000
 
+typedef long long int int64;
+
 struct page_link{
-    int page_id;
+    int64 page_id;
     struct page_link* next;
 };
 
@@ -13,12 +15,13 @@ struct page{
     bool is_used;
     double rank;
     struct page_link* links; // list of links to other pages.
+    int links_count;
 };
 
 struct graph{ // singleton struct to hold all nodes (pages) of the graph
     struct page* array;
     size_t array_size;
-    int pages_count;
+    int64 pages_count;
 };
 
 struct graph pages;
@@ -31,10 +34,12 @@ void graph_init(void){
     pages.pages_count = 0;
     pages.array_size = INIT_GRAPH_NODES;
     pages.array = malloc(INIT_GRAPH_NODES * sizeof(struct page));
-    for(int idx = 0; idx < pages.array_size; idx++){
+    for(int64 idx = 0; idx < pages.array_size; idx++){
         pages.array[idx].links = NULL;
         pages.array[idx].is_used = false;
         pages.array[idx].rank = 1;
+        pages.array[idx].links_count = 0;
+
     }
 }
 
@@ -51,14 +56,15 @@ void graph_double_nodes(void){
         exit(137);
     }
    
-    for(int idx = prev_array_size; idx < pages.array_size; idx++){
+    for(int64 idx = prev_array_size; idx < pages.array_size; idx++){
         pages.array[idx].links = NULL;
         pages.array[idx].is_used = false;
         pages.array[idx].rank = 1;
+        pages.array[idx].links_count = 0;
     }
 }
 
-void graph_add_page_link(int page_id, int page_link){
+void graph_add_page_link(int64 page_id, int64 page_link){
     while(page_id >= pages.array_size){ // if page_id oob: resize the array;
         graph_double_nodes();
     }
@@ -72,12 +78,12 @@ void graph_add_page_link(int page_id, int page_link){
     new_page_link->page_id = page_link;
     new_page_link->next = pages.array[page_id].links;
     pages.array[page_id].links = new_page_link;
-    
+    pages.array[page_id].links_count++;
 }
 
 /* For debugging reasons */
 void graph_print(){
-    for(int idx = 0; idx < pages.array_size; idx++){
+    for(int64 idx = 0; idx < pages.array_size; idx++){
         if(!pages.array[idx].is_used)
             continue;
         printf("%d:\t", idx);
@@ -90,8 +96,39 @@ void graph_print(){
     }
 }
 
+void graph_print_ranks(){
+    printf("node,pagerank\n");
+    for(int64 idx = 0; idx < pages.array_size; idx++){
+        if(!pages.array[idx].is_used)
+            continue;
+        printf("%d:\t%f\n", idx, pages.array[idx].rank);
+    }
+}
+
+void page_rank_single_thread(){
+    int64 pages_visited = 0;
+    int64 idx = 0;
+    while(pages_visited < pages.pages_count){
+        if(!pages.array[idx].is_used){
+            idx++;
+            continue;
+        }
+        struct page_link* ptr = pages.array[idx].links;
+        // increases rank of all neighboring pages. 
+        // (pages that the particular page has links to)
+        double normalized_rank = pages.array[idx].rank / (double)pages.array[idx].links_count;
+        while(ptr){ 
+            pages.array[ptr->page_id].rank += 0.85 * normalized_rank;
+            ptr = ptr->next;
+        }  
+
+        pages_visited++;
+        idx++;
+    }
+}
+
 int main(int argc, const char* argv[]){
-    int page_id, page_link;
+    int64 page_id, page_link;
     size_t size; // unused
     char* str = NULL;
 
@@ -100,11 +137,15 @@ int main(int argc, const char* argv[]){
     while(getline(&str, &size, stdin) != -1){ // currently using stdin
         if(str[0] == '#') // skip comments.
             continue;
-        sscanf(str, "%d\t%d", &page_id, &page_link);
+        sscanf(str, "%ld\t%ld", &page_id, &page_link);
         graph_add_page_link(page_id, page_link);
     }
 
-    graph_print();
+    for(int counter = 0; counter < 2; counter++)
+        page_rank_single_thread();
+
+    //graph_print();
+    graph_print_ranks();
     if(!str)
         free(str);
     graph_free();
